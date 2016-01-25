b4c6274bfc204f20efa9d4b494d55f00a8204125
ngocquyhoang <quyhn@hiworld.com.vn>
2016-01-25 23:51:12 +0700

    update code

--- db/migrate/20160116140229_create_offices.rb
+++ db/migrate/20160116140229_create_offices.rb
@@ -1,11 +1,11 @@
 class CreateOffices < ActiveRecord::Migration
   def change
     create_table :offices do |t|
-      t.text        :title
-      t.text        :owner
-      t.text        :image
-      t.text        :slug
-      t.integer     :like,      null:false, default: 0
+      t.text          :title
+      t.text          :owner
+      t.text          :image
+      t.text          :slug
+      t.integer      :like,          null:false, default: 0
 
       t.timestamps null: false
     end
