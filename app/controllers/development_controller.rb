26427d53448961516bcd38fe7babee58c6cedbc1
ngocquyhoang <ngocquyhoang.93@gmail.com>
2016-02-01 23:43:57 +0700

    update code

--- app/controllers/development_controller.rb
+++ app/controllers/development_controller.rb
@@ -1,6 +1,9 @@
 class DevelopmentController < ApplicationController
 	def index
 		# show message
-		@allMessage = Message.all
+		@allMessage = Message.all.order(created_at: :desc)
+	end
+	def read_message
+		
 	end
 end
