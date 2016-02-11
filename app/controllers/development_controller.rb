a4740d04ab29216d622b2e20e0c80793a893594b
ngocquyhoang <ngocquyhoang.93@gmail.com>
2016-02-11 23:15:57 +0700

    update code

--- app/controllers/development_controller.rb
+++ app/controllers/development_controller.rb
@@ -1,12 +1,26 @@
 class DevelopmentController < ApplicationController
+	# index page
 	def index
+		
+	end
+	# all image page
+	def image
+		
+	end
+	# contact page
+	def contact
+		
+	end
+	# message page
+	def message
 		# show message
 		@allMessage = Message.all.order(created_at: :desc)
 	end
 	def showmessage
 		if params[:idMessage].present?
 			messageQuy = Message.find(params[:idMessage]);
-			render json:{name: messageQuy.name, mail: messageQuy.mail, message: messageQuy.message, send_time: messageQuy.created_at, }
+			# indexbcs = time_ago_in_words(messageQuy.created_at);
+			render json:{name: messageQuy.name, mail: messageQuy.mail, message: messageQuy.message, send_time: messageQuy.created_at.in_time_zone(7).strftime("%B - %d - %Y  %H : %M"), }
 		end
 	end
 	def deletemessage
